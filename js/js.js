c9879a18b5093ed681c964dbe0b29bfb86ffc1a1
enushu <hannahtrishaborsoto@gmail.com>
2024-05-21 16:53:01 +0800

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,11 @@
+function changecolor(){
+    document.getElementById("javascript").style.color ="#cf9fff";
+
+}
+function changecolor2(){
+    document.getElementById("javascript").style.color ="#ffc0cb";
+
+}
+function changeimg(){
+    document.getElementById("BTS").src="img/tulips-hero.jpg";
+}
